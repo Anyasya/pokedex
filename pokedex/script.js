c7c17853e5ea45e405bae137bc5903e64cbcc20d
anyasya <anyasya1999@gmail.com>
2021-08-10 17:08:24 +0200

    v.0.0 little flex

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,5 +1,5 @@
 const btnBurger = document.querySelector('.menu-icon');
-const navHeader = document.querySelector('.header-body');
+const navHeader = document.querySelector('.menu-list');
 const spanBurger = document.querySelectorAll('.span-burger');
 const mainContent = document.querySelector('.main-content');
 
